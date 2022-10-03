c2c376bbba29e1254946bef2dc25ca11bfe11044
Harry Steele <harrysteele@Harrys-MacBook-Air.local>
2022-10-03 15:04:59 -0400

    finished number 11

--- 11_flip_case/flip_case.py
+++ 11_flip_case/flip_case.py
@@ -11,3 +11,33 @@ def flip_case(phrase, to_swap):
         'AaaaHHH'
 
     """
+    upper = to_swap.upper()
+    lower = to_swap.lower()
+    new_string = ""
+
+    for char in phrase:
+        if char == upper or char == lower:
+            new_string += char.swapcase() 
+        else:
+             new_string += char
+    return new_string
+
+    # 1. check if swap char is lower caser - if so check if char is equal to
+        
+
+    
+
+
+
+
+        # if to_swap.islower():
+        #     if char == to_swap:
+        #         char.upper()
+        #     elif char == to_swap.upper():
+        #         char.lower()
+        # elif to_swap.isupper():
+        #     if char == to_swap:
+        #         char.lower()
+        #     elif char == to_swap.lower():
+        #         char.upper()
+        # return phrase
